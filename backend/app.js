8e06ba80b768ba115d9a35ddeeb5740d88a602eb
Xuanyu Zhou <xzhou564@uwo.ca>
2024-11-30 14:27:18 -0500

    Update Shopping Cart Page

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, '../frontend/src')));
 
 
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/src/app', 'cart.html'));
+});
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/src', 'index.html'));
 });
@@ -16,3 +21,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
